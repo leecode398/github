95e34c9f09422a1b78787bf65ce76f187c739f9a
lixiang-code <1411192593@qq.com>
2019-10-23 18:42:19 +0800

    Z型打印字符串

--- pyfile/zString.py
+++ pyfile/zString.py
@@ -1,20 +1,38 @@
-
+import math
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
         l = len(s)
-        rs = list()
-        j = 0
+        if numRows == 1 or l < numRows:
+            return s
+        i = 0
+        m = 1
+        ll = math.ceil(l/(2*numRows-2)*(numRows-1))
+        rs = [['0']*ll for i in range(numRows)]
+        for k in range(ll):
+            if k%(numRows-1) == 0:
+                m -= 1
+                for j in range (numRows):
+                    if i < l and m < numRows:
+                        rs[m][k] = s[i]
+                        i += 1
+                        m += 1
+                m -= 1
+            else:
+                if i < l:
+                    rs[m-1][k] = s[i]
+                    i += 1
+                    m -= 1
+        retstr = list()
         for i in range(numRows):
-            while i < l:
-                print(s[i])
-                rs[j] = s[i]
-                j += 1
-                i += (num-1)
+            for j in range(ll):
+                if rs[i][j] != '0':
+                    retstr.append(rs[i][j])
+        return "".join(retstr)
 
 def main():
     Solution1 = Solution()
-    s = "PAYPALISHIRING"
-    numRows = 3
+    s = "ABC"
+    numRows = 2
     print(Solution1.convert(s, numRows))
 
 if __name__ == "__main__":
